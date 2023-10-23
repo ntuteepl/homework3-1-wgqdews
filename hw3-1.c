814c4de3be5020a4a55403cb26964afdde662e72
wgqdews <146065102+wgqdews@users.noreply.github.com>
2023-10-23 19:34:48 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -6,10 +6,10 @@
 int main(int argc, char *argv[]) {
 	int i,a,b=0;
 	scanf("%d",&a);
-	for(i=2;i<100;i++){
+	for(i=2;i<10000;i++){
 	if(a%i==0 && a!=i){
 		printf("NO");
-		i=100;
+		i=10000;
 		b=1;
 	}
 }
