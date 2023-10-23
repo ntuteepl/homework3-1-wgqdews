0e1e415aeff39323db5efe754899e7f5829ec21c
wgqdews <146065102+wgqdews@users.noreply.github.com>
2023-10-23 18:54:09 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -4,14 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-int num=3,s1,s2,s3,d1,d2,d3;
+int num=0,s1,s2,s3,d1,d2,d3;
 scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-if(s1>=d2||s1>=d3)
-num--;
-if(s2>=d1||s2>=d3)
-num--;
-if(s3>=d1||s3>=d2)
-num--;
+if(s1<d2||s1<d3)
+num++;
+if(s2<d1||s2<d3)
+num++;
+if(s3<d1||s3<d2)
+num++;
 printf("%d",num);
 return 0;
 }
